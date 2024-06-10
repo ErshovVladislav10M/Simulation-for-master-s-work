1488909750a50d73a8984c31a61f73bc9313371b
Vladislav Ershov <ershov.vladislav@huawei-partners.com>
2024-06-10 20:20:39 +0300

    Update coordinate_distribution.py

--- src/distributions/uniform/coordinate_distribution.py
+++ src/distributions/uniform/coordinate_distribution.py
@@ -11,18 +11,12 @@ class UniformCoordinateDistribution(AbstractDistribution):
         self._max_value = max_value
 
     def get_values(self, num_of_values: int) -> list[Coordinate]:
-        x_values = [
-            (self._max_value.x - self._min_value.x) * np.random.random() + self._min_value.x
-            for _ in range(num_of_values)
-        ]
-        y_values = [
-            (self._max_value.y - self._min_value.y) * np.random.random() + self._min_value.y
-            for _ in range(num_of_values)
-        ]
-        z_values = [
-            (self._max_value.z - self._min_value.z) * np.random.random() + self._min_value.z
-            for _ in range(num_of_values)
-        ]
+        x = self._max_value.x - self._min_value.x
+        x_values = [x * np.random.random() + self._min_value.x for _ in range(num_of_values)]
+        y = self._max_value.y - self._min_value.y
+        y_values = [y * np.random.random() + self._min_value.y for _ in range(num_of_values)]
+        z = self._max_value.z - self._min_value.z
+        z_values = [z * np.random.random() + self._min_value.z for _ in range(num_of_values)]
 
         return [
             Coordinate(x, y, z)
