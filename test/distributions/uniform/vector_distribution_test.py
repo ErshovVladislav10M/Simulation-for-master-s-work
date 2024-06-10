6844c3eafbff3f6cccca18e1d8f4dfa29386de76
Vladislav Ershov <ershov.vladislav@huawei-partners.com>
2024-06-10 20:01:27 +0300

    Update distribution tests

--- test/distributions/uniform/vector_distribution_test.py
+++ test/distributions/uniform/vector_distribution_test.py
@@ -9,16 +9,13 @@ def test():
         max_value=Vector.of(Coordinate(100, 100, 100))
     )
     values = distribution.get_values(num_of_values=1000)
-    x_values = [vector.x for vector in values]
-    y_values = [vector.y for vector in values]
-    z_values = [vector.z for vector in values]
 
     assert len(values) == 1000
 
-    assert min(x_values) >= 10
-    assert min(y_values) >= 10
-    assert min(z_values) >= 10
+    assert min([vector.x for vector in values]) >= 10
+    assert min([vector.y for vector in values]) >= 10
+    assert min([vector.z for vector in values]) >= 10
 
-    assert max(x_values) <= 100
-    assert max(y_values) <= 100
-    assert max(z_values) <= 100
+    assert max([vector.x for vector in values]) <= 100
+    assert max([vector.y for vector in values]) <= 100
+    assert max([vector.z for vector in values]) <= 100
